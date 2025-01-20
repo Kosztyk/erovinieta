566df28b7f4f80ac68835c4e2fdb188dbd1b172c
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2025-01-20 19:38:01 +0200

    Update __init__.py

--- custom_components/erovinieta/__init__.py
+++ custom_components/erovinieta/__init__.py
@@ -2,12 +2,15 @@
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
 from datetime import timedelta
 import logging
 from .const import DOMAIN, CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL
 from .coordinator import ErovinietaCoordinator
 from .api import ErovinietaAPI
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 _LOGGER = logging.getLogger(__name__)
 
